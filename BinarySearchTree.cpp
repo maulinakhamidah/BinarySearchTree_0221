e0d89b60712cf70f87e70ad334f893ba8fe0aa58
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-06-24 10:54:41 +0700

    membuat node baru

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -32,4 +32,7 @@ class BinaryTree
         cout << "Masukkan nilai: ";
         cin >> x;
 
-        
+        // step 1: allocate memory for the new node
+        Node *newNode = new Node();
+
+       
